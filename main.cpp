70467399cddacdbf244e34ee68ccebc87bd708d6
Rick Warrington <phial1020@gmail.com>
2011-08-09 14:00:19 -0600

    Removed all cout statements. All output is done through MessageBox()'s

--- main.cpp
+++ main.cpp
@@ -22,17 +22,12 @@ int main(int argc, char * argv[]) {
 	config.open("config");
 
 	if(config.fail()) {
-		cout << "Failed to open 'config'.  Terminating application\n";
-		cout << "Please make sure the file exists in the same directory as the application.\n";
-		cout << "Press ENTER to quit.\n";
-		cin.get();
+		MessageBox(NULL, "Failed to open config.\nPlease make sure the file exists in the application directory.\n\nPress OK to exit.", "Error", MB_ICONERROR | MB_OK);
 		return 1;
 	}
 
 	config.getline(configpath, 256, '\n');
 	config.close();
-
-	cout << "Using path:\n\t" << configpath << "\n\n";
 
 	strcpy(inputfilepath, configpath);
 	strcpy(outputpath, configpath);
@@ -44,32 +39,33 @@ int main(int argc, char * argv[]) {
 	combatlog.open(inputfilepath);
 
 	if(combatlog.fail()) {
-		cout << "Could not find a combat log at:\n\t" << inputfilepath << "\n\n";
+		char errorMessage[256] = "Could not find a combat log at '";
+		strcat(errorMessage, inputfilepath);
+		strcat(errorMessage, "'.");
+		MessageBox(NULL, errorMessage, "Error", MB_ICONWARNING | MB_OK);
 	}
 	combatlog.close();
 
 	if(CopyFile(inputfilepath, outputpath, false) != 0) {
-		cout << "Successfully created new combat log! \n\t" << outputpath << "\n\n";
+		char successMessage[256] = "Successfully created a new combat log at '";
+		strcat(successMessage, outputpath);
+		strcat(successMessage, "'.");
+		MessageBox(NULL, successMessage, "", MB_ICONINFORMATION | MB_OK);
 	}
 	else {
-		cout << "Failed to create new combat log!\n\tError " << GetLastError() << "\n\n";
-		cout << "Press ENTER to quit.\n";
-		cin.get();
+		MessageBox(NULL, "Failed to create new combat log.\nAborting.", "Error", MB_ICONERROR | MB_OK);
 		return 1;
 	}
 
-	cout << "Deleting old combat log.\n";
 	int deleteSuccess = NULL;
 	deleteSuccess = DeleteFile(inputfilepath);
 
 	if(deleteSuccess != 0) {
-		cout << "Successfully deleted old combatlog.\n";
+		MessageBox(NULL, "Successfully removed old combatlog file.", "", MB_ICONINFORMATION | MB_OK);
 	}
 	else {
-		cout << "Failed to delete old combatlog.  You need to delete it yourself.\n";
+		MessageBox(NULL, "Failed to delete old combatlog file.  Did you forget to close World of Warcraft?\nYou will need to remove the old combatlog manually or new logs will\nbe appended to the end.\n\nThe file cannot be deleted while it is open for writing by another application.\nNext time try closing World of Warcraft before running this application.", "Error", MB_ICONWARNING | MB_OK);
 	}
 
-	cout << "Press ENTER to quit.\n";
-	cin.get();
 	return 0;
 }
